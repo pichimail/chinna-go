e76bd6781dbd95abb3a0444c9588d3a04aa56548
pichimail <pichimail24@gmail.com>
2026-06-11 11:15:23 +0530

    chore: sync extension artifacts

--- extension/dist/content.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  if (window.top !== window) return;
-  if (window.__chinnaRuntimeHookRequested) return;
-  window.__chinnaRuntimeHookRequested = true;
-  chrome.runtime.sendMessage({ type: 'install-runtime-hooks' }, () => void chrome.runtime.lastError);
-})();
